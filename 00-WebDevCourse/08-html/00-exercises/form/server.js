3bc50daf8830ac509f0b6553d3d88588f531bc75
Aliphinos-Fylakas <pessoal.carlosveras@gmail.com>
2023-02-05 22:24:43 -0300

    Conclusion of html module of the modern web course

--- 00-WebDevCourse/08-html/00-exercises/form/server.js
+++ 00-WebDevCourse/08-html/00-exercises/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post("/users", (req, ans) => {
     console.log(req.body)
-    ans.send("<h1>Congratulations</h1>")
+    ans.send("<h1>Congratulations.</h1><br><h2>Included user!</h2>")
+})
+
+app.post("/users/:id", (req, ans) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    ans.send("<h1>Congratulations.</h1>\n<h2>Modified user!</h2>")
 })
 
 app.listen(5500)
