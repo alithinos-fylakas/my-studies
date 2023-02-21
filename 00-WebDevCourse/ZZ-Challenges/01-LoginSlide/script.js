3492d9aa33fafb1753ecd0a277b3782e3e455af3
Aliphinos-Fylakas <pessoal.carlosveras@gmail.com>
2023-02-21 18:37:39 -0300

    Creating a login-screen with an automatic slide

---
+++ 00-WebDevCourse/ZZ-Challenges/01-LoginSlide/script.js
@@ -0,0 +1,15 @@
+
+function slide() {
+    let i
+    let slides = document.getElementsByClassName("image")
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+    slideIndex++
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block"
+    setTimeout(slide, 2000)
+}
+
+let slideIndex = 0
+slide()
